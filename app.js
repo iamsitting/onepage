e07ef995353bc6b46bb581481ef3c683d9c5e3f4
iamsitting <cdsalamanca27@gmail.com>
2018-07-30 14:16:48 -0500

    use node modules

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('static'));
+app.use(express.static('node_modules'));
+
 
 var server = app.listen(8081, function() {
   var host = server.address().address;

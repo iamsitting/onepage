61a767274ea0d071bee624ed85fd9d83b062dbb2
iamsitting <cdsalamanca27@gmail.com>
2018-07-30 16:04:33 -0500

    navbar fixes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var ScrollTriggger = require("scrolltrigger-classes");
 var app = express();
 
 app.use(express.static('static'));
--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,5 @@ $(() => {
     var $nav = $(".navbar-fixed-top");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
+
 });
